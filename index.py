d8c2079aeccbb87401ba700e99213a32793981a1
Viraj Rank <virajrank48@gmail.com>
2025-12-20 18:27:55 +0530

    file added

--- index.py
+++ index.py
@@ -1,1 +1,11 @@
 print("This is the index.py file in the LocalRepo directory.")
+
+# Econometric Dummy dependent Variable regression
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import statsmodels.api as sm
+from statsmodels.formula.api import logit
+
+df = pd.read_csv('C:\\Users\\dell\\OneDrive\\Documents\\prectice data.csv')
+
